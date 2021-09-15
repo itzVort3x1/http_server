026ac49d097bf38b0b0d9f9e6670d55bcf3c8807
Kaustubh <saikaustubh10@gmail.com>
2021-09-15 08:12:21 +0530

    added traits to handle request

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -26,6 +29,13 @@ impl Server {
                                         "Received a request: {}",
                                         String::from_utf8_lossy(&buffer)
                                    );
+
+                                   match Request::try_from(&buffer[..]) {
+                                        Ok(request) => {}
+                                        Err(e) => {
+                                             println!("Failed to parse a request: {}", e)
+                                        }
+                                   }
                               }
                               Err(e) => {
                                    println!("Failed to read from connection: {}", e)
